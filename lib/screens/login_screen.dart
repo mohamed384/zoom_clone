ed92c90b2122e054832e25b3690cf838ce26b0b5
medoezz519 <me7717180@gmail.com>
2022-06-29 22:31:21 +0200

    zoom update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/widgets/custom_button.dart';
+
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,6 +11,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text(
-            'Start or Join Meeting',
+            'Start or join a meeting',
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -26,7 +30,15 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: const EdgeInsets.symmetric(vertical: 38.0),
             child: Image.asset('assets/images/onboarding.jpg'),
           ),
-           CustomButton(text: 'Login',onPressed: (){}),
+          CustomButton(
+            text: 'Google Sign In',
+            onPressed: () async {
+              bool res = await _authMethods.signInWithGoogle(context);
+              if (res) {
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          ),
         ],
       ),
     );
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,27 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:zoom_clone/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.text, required this.onPressed}) : super(key: key);
   final String text;
   final VoidCallback onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
           primary: buttonColor,
-        minimumSize: const Size(double.infinity, 50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-          side: const BorderSide(color: buttonColor),
-        )
-      ),
-      child: Text(text,
-      style: const TextStyle(
-        fontSize: 17
-      ),),
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 17,
+          ),
+        ),
       ),
     );
   }
